7760a4ed08a21b15f7f875f00e5c38b65c6a1698
Michael Russo <russo.michaelp@gmail.com>
2020-03-10 19:57:30 -0400

    feat: Create book model and preliminary routes (#1)

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/book.model');
+
+mongoose.connect('mongodb://localhost/library', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
+app.use(express.json());
 
-app.get('/ping', (req, res) => {
+app.get('/api/ping', (req, res) => {
   res.send('pong');
 });
+
+app
+  .route('/api/books')
+  .get(async (req, res) => {
+    const books = await Book.find();
+    res.json(books);
+  })
+  .post(async (req, res) => {
+    const result = await Book.create(req.body);
+    res.json(result);
+  });
 
 app.listen(3000, () => {
   console.log('app listening at http://localhost:3000');
