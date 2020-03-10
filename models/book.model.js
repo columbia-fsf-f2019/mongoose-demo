b7ff45a5159432ec58abb1c5b51cbe0effd55b6a
russomp <russo.michaelp@gmail.com>
2020-03-10 19:53:13 -0400

    feat: Create book model and preliminary routes

---
+++ models/book.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const BookSchema = new Schema({
+  author: String,
+  title: String
+});
+
+const Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
