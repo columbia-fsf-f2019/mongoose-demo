66fe6a306ffc4be98fa2f3674c8d1d7ea4e03af5
russomp <russo.michaelp@gmail.com>
2020-03-10 21:03:29 -0400

    feat:Track books in each library and enable users to add books to a given library

--- server.js
+++ server.js
@@ -15,16 +15,14 @@ app.get('/api/ping', (req, res) => {
   res.send('pong');
 });
 
-app
-  .route('/api/books')
-  .get(async (req, res) => {
-    const books = await Book.find();
-    res.json(books);
-  })
-  .post(async (req, res) => {
-    const result = await Book.create(req.body);
-    res.json(result);
-  });
+app.route('/api/books').get(async (req, res) => {
+  const books = await Book.find();
+  res.json(books);
+});
+// .post(async (req, res) => {
+//   const result = await Book.create(req.body);
+//   res.json(result);
+// });
 
 app
   .route('/api/libraries')
@@ -35,6 +33,24 @@ app
   .post(async (req, res) => {
     try {
       const result = await Library.create(req.body);
+      res.json(result);
+    } catch (err) {
+      res.json(err);
+    }
+  });
+
+app
+  .route('/api/libraries/:id/books')
+  .get(async (req, res) => {
+    const library = await Library.findById(req.params.id).populate('books');
+    res.json(library.books);
+  })
+  .post(async (req, res) => {
+    try {
+      const book = await Book.create(req.body);
+      const result = await Library.findByIdAndUpdate(req.params.id, {
+        $push: { books: book._id }
+      });
       res.json(result);
     } catch (err) {
       res.json(err);
